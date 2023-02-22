c04616b0dbd468bc470138699ebce01cc4aeeb15
7dhaval <dhavalsinhsonasaniya7318@gmail.com>
2023-02-22 23:54:14 +0530

    add joi validation schema

--- Routes/auth.route.js
+++ Routes/auth.route.js
@@ -2,24 +2,28 @@ const express = require("express");
 const createHttpError = require("http-errors");
 const router = express.Router();
 const User = require("../Modules/user.module");
+const { authSchema} = require('../helpers/validation.schma');
 
 
 router.use(express.json());
 router.post('/register', async (req, res) => {
     try {
-        const email = req.body.email;
-        const password = req.body.password;
+        // const email = req.body.email;
+        // const password = req.body.password;
+        // const { email, password } = req.body
+        const result = await authSchema.validateAsync(req.body)
 
-        if(!email || !password) throw createHttpError.BadRequest();
+        // if(!email || !password) throw createHttpError.BadRequest();
 
-       const doesExist =  await User.findOne({email});
+       const doesExist =  await User.findOne({email : result.email});
        if (doesExist) throw createHttpError.Conflict(`${email} is already registered`);
 
-       const user = new User ({ email, password});
+       const user = new User (result);
        const savedUser = await user.save();
        res.send(savedUser);
 
     } catch (error) {
+        if(error.isJoi === true)error.status =422
        console.log(error);
     }
 })
